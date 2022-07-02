883a85c1fab3c301d6cd072ad44a9977937c9bbd
Sergey68 <sesergey@yandex.ru>
2022-07-02 18:57:09 +1100

    Example1 added

---
+++ Example1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+void printRangeXY(float PlaneNum)
+{
+    if (PlaneNum == 1) 
+    {
+        System.Console.WriteLine("Х положительный");
+        System.Console.WriteLine("Y положительный");
+    }
+        
+    else if (PlaneNum == 2)
+    {
+        System.Console.WriteLine("Х отрицательный");
+        System.Console.WriteLine("Y положительный");
+    }
+        
+    else if (PlaneNum == 3)
+    {
+        System.Console.WriteLine("Х отрицательный");
+        System.Console.WriteLine("Y отрицательный");
+    }
+       
+    else if (PlaneNum == 4)
+    {
+        System.Console.WriteLine("Х положительный");
+        System.Console.WriteLine("Y отрицательный");
+        
+    }
+        
+    else 
+    System.Console.WriteLine("Некорректный ввод данных");
+}
+printRangeXY (1);
