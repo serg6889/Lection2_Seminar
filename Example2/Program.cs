042962cab220b178e913675ff0324f1457b25e76
Sergey68 <sesergey@yandex.ru>
2022-07-03 16:20:36 +1100

    File is updated with Task-1

---
+++ Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите координаты А ");
+int AX = Convert.ToInt32(Console.ReadLine());
+int AY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты B ");
+int BX = Convert.ToInt32(Console.ReadLine());
+int BY = Convert.ToInt32(Console.ReadLine());
+
+int dX = AX - BX;
+int dY = AY - BY;
+
+
+Double rangeC = Math.Sqrt(dX * dX + dY * dY);
+System.Console.WriteLine(rangeC);
+
